a04b646e573c1e309c4450b1e4cb106bd7850f06
zhuima <zhuima314@gmail.com>
2022-07-22 11:03:41 +0800

    add react-icons dependencies

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,19 @@
-import React from "react";
-import "./index.scss";
+import React from 'react'
+import { AiFillGithub, AiFillGoogleCircle } from 'react-icons/ai'
+import './index.scss'
 
 const Footer = () => (
   <footer className="footer">
-    <p>
-      {" "}
-      &copy; {new Date().getFullYear()} Copyright:{" "}
-      <a className="text-dark" href="https://github.com/zhuima">
-        zhuima
+    <p> &copy; {new Date().getFullYear()} Copyright: zhuima</p>
+    <div className="icons">
+      <a href="https://github.com/zhuima/awesome-my-learn-roadmap">
+        <AiFillGithub style={{ color: '#333333', fontSize: '30px' }} />
+      </a>{' '}
+      <a href="mailto:zhuima314@gmail.com">
+        <AiFillGoogleCircle style={{ color: '#dd4b39', fontSize: '30px' }} />
       </a>
-    </p>
+    </div>
   </footer>
-);
+)
 
-export default Footer;
+export default Footer
