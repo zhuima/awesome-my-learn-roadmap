67d5cead3ad8faa0a7944bfebac4a14ea4bb5beb
zhuima <zhuima314@gmail.com>
2022-07-25 17:27:35 +0800

    add time display

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import rocket from './assets/rocket.svg'
 import TextTransition, { presets } from 'react-text-transition'
 import data from './data/data.json'
@@ -7,20 +7,33 @@ import Footer from './components/Footer/Footer'
 import './App.scss'
 
 function App() {
-  const [index, setIndex] = React.useState(0)
+  const [index, setIndex] = useState(0)
+  const [time, setTime] = useState(new Date().toLocaleString())
   const TEXTS = [
     '欢迎提交你认为的优质站点或文档',
     '日常收集优质站点',
     '期待你的参与',
   ]
 
-  React.useEffect(() => {
+  useEffect(() => {
     const intervalId = setInterval(
       () => setIndex((index) => index + 1),
       5000 // every 3 seconds
     )
     return () => clearTimeout(intervalId)
   }, [])
+
+  useEffect(() => {
+    const interval = setInterval(
+      showCurrTime,
+      1000 // every 1 second
+    )
+    return () => clearInterval(interval)
+  }, [])
+
+  const showCurrTime = () => {
+    setTime(new Date().toLocaleString())
+  }
 
   return (
     <div className="App">
@@ -31,6 +44,7 @@ function App() {
             {TEXTS[index % TEXTS.length]}
           </TextTransition>
         </span>
+        <p className="Time">{time}</p>
       </h6>
 
       <Items data={data} />
