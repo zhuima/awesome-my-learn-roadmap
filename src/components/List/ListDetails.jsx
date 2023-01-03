5f9556e3eddde4a02920722b77f6e6fb2227303d
zhuima <zhuima314@gmail.com>
2023-01-03 13:57:47 +0800

    Feature: add tooltip function

--- src/components/List/ListDetails.jsx
+++ src/components/List/ListDetails.jsx
@@ -1,7 +1,9 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import { bgcolor } from "../../utils/bgcolor";
+import { Tooltip as ReactTooltip } from "react-tooltip";
 
+import "react-tooltip/dist/react-tooltip.css";
 import "./index.scss";
 const ItemDetails = ({ items }) => {
   return (
@@ -14,10 +16,18 @@ const ItemDetails = ({ items }) => {
             key={index}
             className="item"
             style={{ background: random_bg_color }}
+            id={msg}
           >
             <a href={href}>
               <i>{msg}</i>
             </a>
+
+            <ReactTooltip
+              anchorId={item.msg}
+              place="top"
+              variant="success"
+              content={item.comments}
+            />
           </div>
         );
       })}
