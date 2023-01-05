dfc4644a6433fb2a8818a540634dffaf8016ea2a
zhuima <zhuima314@gmail.com>
2023-01-05 10:35:46 +0800

    Fix: husky not work

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import ScrollToTop from 'react-scroll-to-top';
-
 import rocket from '@/assets/rocket.svg';
 import TextTransition, { presets } from 'react-text-transition';
 import data from '@/data/data.json';
