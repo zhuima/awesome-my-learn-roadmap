1e7a61bcc4265877fb2d9ac779bce07b4fff611d
zhuima <zhuima314@gmail.com>
2023-01-05 10:29:24 +0800

    Fix: husky not work

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,15 @@ import Items from '@/components/List/List';
 import Footer from '@/components/Footer/Footer';
 import '@/App.scss';
 
+const TEXTS = [
+  '欢迎提交你认为的优质站点或文档',
+  '日常收集优质站点',
+  '期待你的参与',
+];
+
 function App() {
   const [index, setIndex] = useState(0);
   const [time, setTime] = useState(new Date().toLocaleString());
-  const TEXTS = [
-    '欢迎提交你认为的优质站点或文档',
-    '日常收集优质站点',
-    '期待你的参与',
-  ];
 
   useEffect(() => {
     const intervalId = setInterval(
